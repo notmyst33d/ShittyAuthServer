0c1f6ebb51d5bfa5cc9c66b07fe5e0b3cd5d6a80
Myst33d <notmyst33d@gmail.com>
2022-04-28 21:32:40 +0600

    Make /refresh more usable and stable

--- src/me/mrletsplay/shittyauth/page/api/yggdrasil/RefreshPage.java
+++ src/me/mrletsplay/shittyauth/page/api/yggdrasil/RefreshPage.java
@@ -1,9 +1,11 @@
 package me.mrletsplay.shittyauth.page.api.yggdrasil;
 
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 import me.mrletsplay.mrcore.json.JSONObject;
 import me.mrletsplay.shittyauth.ShittyAuth;
+import me.mrletsplay.shittyauth.UUIDHelper;
 import me.mrletsplay.shittyauth.auth.AccessToken;
 import me.mrletsplay.shittyauth.auth.StoredAccessToken;
 import me.mrletsplay.webinterfaceapi.http.HttpStatusCodes;
@@ -11,6 +13,7 @@ import me.mrletsplay.webinterfaceapi.http.document.HttpDocument;
 import me.mrletsplay.webinterfaceapi.http.header.HttpClientContentTypes;
 import me.mrletsplay.webinterfaceapi.http.request.HttpRequestContext;
 import me.mrletsplay.webinterfaceapi.webinterface.Webinterface;
+import me.mrletsplay.webinterfaceapi.webinterface.auth.WebinterfaceAccount;
 
 public class RefreshPage implements HttpDocument {
     // https://wiki.vg/Authentication#Refresh
@@ -22,15 +25,24 @@ public class RefreshPage implements HttpDocument {
         String accessToken = obj.getString("accessToken"),
                 clientToken = obj.optString("clientToken").orElse(null);
 
-        Webinterface.getLogger().info("Somebody requested /refresh, please note that this is an experimental feature.");
-
         StoredAccessToken sTok = ShittyAuth.tokenStorage.getStoredToken(accessToken);
         if (sTok == null) {
             ctx.getServerHeader().setStatusCode(HttpStatusCodes.ACCESS_DENIED_403);
             return;
         }
 
+        WebinterfaceAccount account = Webinterface.getAccountStorage().getAccountByID(sTok.getAccountID());
+        if (account == null) { // I dont know how would that even happen, but lets check it anyway
+            ctx.getServerHeader().setStatusCode(HttpStatusCodes.ACCESS_DENIED_403);
+            return;
+        }
+
         JSONObject response = new JSONObject();
+        JSONObject profile = new JSONObject();
+        profile.put("name", account.getName());
+        profile.put("id", UUIDHelper.toShortUUID(UUID.fromString(account.getID())));
+
+        response.put("selectedProfile", profile);
 
         AccessToken tok = ShittyAuth.tokenStorage.generateToken(sTok.getAccountID(), clientToken);
         response.put("accessToken", tok.getAccessToken());
